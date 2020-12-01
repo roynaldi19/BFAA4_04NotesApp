d20e86efd1a31f2772ff494b92f5ea89f627ca9f
kidd <roynaldi19@gmail.com>
2020-12-01 15:39:54 +0700

    ini commit pertama

---
+++ app/src/main/java/com/roynaldi19/bfaa4_04notesapp/helper/MappingHelper.kt
@@ -0,0 +1,24 @@
+package com.roynaldi19.bfaa4_04notesapp.helper
+
+import android.database.Cursor
+import com.roynaldi19.bfaa4_04notesapp.Note
+import com.roynaldi19.bfaa4_04notesapp.db.DatabaseContract
+
+
+object MappingHelper {
+
+    fun mapCursorToArrayList(notesCursor: Cursor?): ArrayList<Note> {
+        val notesList = ArrayList<Note>()
+
+        notesCursor?.apply {
+            while (moveToNext()) {
+                val id = getInt(getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
+                val title = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+                val description = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION))
+                val date = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+                notesList.add(Note(id, title, description, date))
+            }
+        }
+        return notesList
+    }
+}
